ca5643e05f5fb780de513f446f66fdb78989c86a
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-05-13 15:02:04 -0700

    Add basic rendering of instance properties

--- lib/filters/api_json_filter.rb
+++ lib/filters/api_json_filter.rb
@@ -4,24 +4,100 @@ require 'json'
 class ApiJsonFilter < Nanoc::Filter
   identifier :api_json
 
-  def octicon(name)
-    "<span class=\"octicon octicon-#{name}\"></span>"
+  # ----- Helpers -----
+
+  def class_source_link(data, params)
+    source_link(data, "class")
+  end
+
+  def property_source_link(data, params)
+    source_link(data, "property")
+  end
+
+  def source_link(item, type = nil)
+    <<~HTML
+      <a
+        class="document-source"
+        href="#{item["srcUrl"]}"
+        #{type ? " title=\"View #{type} source\"" : ""}>
+        #{octicon("file-code")}
+      </a>
+    HTML
   end
 
   def markdown(text)
     GitHub::Markdown.render(text)
   end
 
-  def class_source_link(data, params)
+  def octicon(name)
+    "<span class=\"octicon octicon-#{name}\"></span>"
+  end
+
+  def argument(arg)
+    "<span class=\"argument\">#{arg["name"]}</span>"
+  end
+
+  def argument_list(func)
+    args = func["arguments"]
+
     <<~HTML
-      <a
-        class="document-source"
-        href="#{data["srcUrl"]}"
-        title="View class source">
-        #{octicon("file-code")}
-      </a>
+      <span class="argument-list">(#{args ? args.map { |arg| argument(arg) }.join(", ") : ""})</span>
     HTML
   end
+
+  def method(func, scope, params = {})
+    <<~HTML
+      <div
+        class="api-entry js-api-entry #{visibility_class(func["visibility"])}"
+        id="#{scope}-#{func["name"]}">
+        <h3 class="name">
+          #{func["name"]}#{argument_list(func)}
+          #{source_link(func)}
+        </h3>
+      </div>
+    HTML
+  end
+
+  def property(prop, scope, params = {})
+    <<~HTML
+      <div
+        class="api-entry js-api-entry #{visibility_class(prop["visibility"])}"
+        id="#{scope}-#{prop["name"]}">
+        <h3 class="name">
+          #{prop["name"]}
+          #{property_source_link(prop, params)}
+        </h3>
+        <div>
+          <div class="summary markdown-body">
+            #{markdown(prop["summary"])}
+          </div>
+        </div>
+      </div>
+    HTML
+  end
+
+  def visibility(viz)
+    case viz
+    when "Public" then "Essential"
+    else viz
+    end
+  end
+
+  def visibility_class(viz)
+    visibility(viz).downcase
+  end
+
+  def visibility_label(data, params)
+    <<~HTML
+      <span
+        class="label label-#{visibility_class(data["visibility"])}"
+        title="This class is in the #{visibility_class(data["visibility"])} API">
+        #{data["visibility"]}
+      </span>
+    HTML
+  end
+
+  # ----- Page Sections -----
 
   def description(data, params)
     markdown(data["description"])
@@ -39,22 +115,17 @@ class ApiJsonFilter < Nanoc::Filter
 
   def sections(data, params = {})
     section_text = data["sections"].map do |section|
+      props = data["instanceProperties"].select { |prop| prop["sectionName"] == section["name"] }
+      methods = data["instanceMethods"].select { |func| func["sectionName"] == section["name"] }
+
       <<~HTML
         <h2 class="detail-section">#{section["name"]}</h2>
+        #{props.map { |prop| property(prop, "instance", params) }.join}
+        #{methods.map { |func| method(func, "instance", params) }.join}
       HTML
     end
 
     section_text.join("\n")
-  end
-
-  def visibility_label(data, params)
-    <<~HTML
-      <span
-        class="label label-#{data["visibility"].downcase}"
-        title="This class is in the #{data["visibility"].downcase} API">
-        #{data["visibility"]}
-      </span>
-    HTML
   end
 
   def run(content, params = {})
