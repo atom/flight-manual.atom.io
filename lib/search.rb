46853790fbbb793426d8d971cc8014ef68deace3
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-01 15:24:25 -0700

    Add a section to the table of contents for latest version class names

--- lib/search.rb
+++ lib/search.rb
@@ -5,8 +5,17 @@ def latest_atom_version_apis
   items.map { |item| JSON.parse(File.read(item.raw_filename)) }
 end
 
+def latest_atom_version_class_names
+  memoize('latest_atom_version_class_names') do
+    @items.
+      find_all("/api/v#{latest_atom_version_number}/*.json").
+      map { |item| File.basename(item.raw_filename, ".json") }.
+      sort
+  end
+end
+
 def latest_atom_version_number
-  @latest_atom_version_number ||= begin
+  memoize('latest_atom_version_number') do
     versions = @items.find_all('/api/*/*.json').map { |item| item.path.split('/')[2] }.uniq
 
     exploded_version_numbers = versions.map { |version|
@@ -15,5 +24,31 @@ def latest_atom_version_number
     }
 
     exploded_version_numbers.sort.last.join('.')
+  end
+end
+
+def memoize(key)
+  if temp_exists?(key)
+    temp_read(key)
+  else
+    value = yield
+
+    temp_write(key, value)
+
+    value
+  end
+end
+
+def temp_exists?(name)
+  File.exists?(File.join(Dir.pwd, "tmp", name))
+end
+
+def temp_read(name)
+  File.readlines(File.join(Dir.pwd, "tmp", name))
+end
+
+def temp_write(name, value)
+  File.open(File.join(Dir.pwd, "tmp", name), "w") do |f|
+    f.puts(value)
   end
 end
