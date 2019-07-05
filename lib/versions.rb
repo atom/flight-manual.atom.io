d8470a057d5c2eb9c47e76d709d01dffbc7e3a20
Antonio Scandurra <as-cii@github.com>
2019-07-05 10:20:29 +0200

    Honor selected version during search

--- lib/versions.rb
+++ lib/versions.rb
@@ -1,7 +1,7 @@
 require('json')
 
-def latest_atom_version_apis
-  items = @items.find_all("/api/v#{latest_atom_version_number}/*.json")
+def atom_version_apis(version)
+  items = @items.find_all("/api/v#{version}/*.json")
   items.map { |item| JSON.parse(File.read(item.raw_filename)) }
 end
 
@@ -15,14 +15,16 @@ def latest_atom_version_class_names
 end
 
 def latest_atom_version_number
-  temp_memoize('latest_atom_version_number') do
+  atom_versions.last.to_s
+end
+
+def atom_versions
+  temp_memoize('atom_versions') do
     @items
       .find_all('/api/*/*.json')
       .map { |item| File.basename(File.dirname(item.identifier)) }
       .uniq
       .map { |item| Gem::Version.new(item.tr('v', '')) }
       .sort
-      .last
-      .to_s
   end
 end
